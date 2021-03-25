1a18893f7ac75618274804811469e11a7b5dc7e0
Alexandre Munis <aalexandrm456@gmail.com>
2021-03-25 15:27:17 -0300

    Desenvolvimento da rota 'findEmployeebyId'.

--- employee-api/src/controllers/employee.controller.js
+++ employee-api/src/controllers/employee.controller.js
@@ -11,7 +11,7 @@ const db = require('../config/database')
 exports.createEmployee = async(req, res) => {
   const { name, job_role, salary, birth, employee_registration } = req.body
   const { rows } = await db.query(
-    "INSERT INTO employee (name, job_role, salary, birth, employee_registration) VALUES ($1, $2, $3, $4, $5)",
+    'INSERT INTO employee (name, job_role, salary, birth, employee_registration) VALUES ($1, $2, $3, $4, $5)',
     [name, job_role, salary, birth, employee_registration]
   )
 
@@ -23,11 +23,22 @@ exports.createEmployee = async(req, res) => {
   })
 }
 
-// ==> Método responsável por lsitar todos os 'Employees':
+// ==> Método responsável por listar todos os 'Employees':
 exports.listAllEmployees = async(req, res) => {
   const response = await db.query(
-    "SELECT * FROM employee ORDER BY name ASC"
+  'SELECT * FROM employee ORDER BY name ASC'
   )
 
   res.status(201).send(response.rows)
+}
+
+// ==> Método responsável por listar um determinador 'Employee' por ID:
+exports.findEmployeeById = async(req, res) => {
+  const employeeId = req.params.id
+  const response = await db.query(
+    'SELECT * FROM employee WHERE employee_id = $1',
+    [employeeId]
+  )
+
+  res.status(200).send(response.rows)
 }
