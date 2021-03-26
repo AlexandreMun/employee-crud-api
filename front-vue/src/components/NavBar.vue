2a80913f193cd4c422d78a5f1e6716bfe501b535
Alexandre Munis <aalexandrm456@gmail.com>
2021-03-26 08:21:30 -0300

    Base da home Vue.js.

---
+++ front-vue/src/components/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+    <!-- INICIO NAVBAR -->
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+      <div class="container">
+        <a href="" class="navbar-brand order-md-last">CRUD Vue.js + PostgreSQL + Node.js</a>
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <routerLink class="nav-link" to="/">Create New Employee</routerLink>
+          </li>
+        </ul>
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <routerLink class="nav-link" to="/list-employees">List All Employees</routerLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <!-- FIM NAVBAR -->
+</template>
--- front-vue/src/main.js
+++ front-vue/src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue';
+import VueSweetAlert2 from 'vue-sweetalert2';
 import App from './App.vue';
 import router from './router';
 
-createApp(App).use(router).mount('#app');
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const app = createApp(App);
+app.use(router, VueSweetAlert2);
+app.mount('#app');
