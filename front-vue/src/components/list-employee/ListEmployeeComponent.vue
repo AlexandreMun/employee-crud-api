7277a7836e157c0a935c84a5329909a5aab7695e
Alexandre Munis <aalexandrm456@gmail.com>
2021-04-01 15:33:59 -0300

    Desenvolvimento da função 'deletar'

--- front-vue/src/components/list-employee/ListEmployeeComponent.vue
+++ front-vue/src/components/list-employee/ListEmployeeComponent.vue
@@ -27,7 +27,7 @@
             </router-link>
           </td>
           <td>
-            <button class="btn btn-danger">
+            <button @click="removeEmployee(employee.employee_id)" class="btn btn-danger">
               <font-awesome-icon :icon="['fas', 'trash']"/> Delete
             </button>
           </td>
@@ -43,17 +43,41 @@ import EmployeeService from '../../services/EmployeeService';
 
 export default {
   data() {
+    onMounted(() => {
+      this.listAllEmployees();
+    });
+
     return {
       employees: [],
     };
-  },
-  mounted() {
-    this.listAllEmployees();
   },
   methods: {
     async listAllEmployees() {
       const response = await EmployeeService.getEmployees();
       this.employees = response;
+    },
+    async removeEmployee(id) {
+      this.$swal({
+        title: 'Are you sure you want to delete the employee?',
+        text: 'Watch out! This Employee will be deleted!',
+        icon: 'warning',
+        showConfirmButton: true,
+        allowOutsideClick: false,
+        allowEnterKey: true,
+        allowEscapeKey: false,
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes! Please, delete it!',
+      }).then(async (result) => {
+        if (result.value) {
+          await EmployeeService.deleteEmployee(id);
+          this.$swal('Deleted', 'Sucessfully Deleted', 'success');
+          this.listAllEmployees();
+        } else {
+          this.$swal('Cancelled', 'Cancel Deletion', 'info');
+        }
+      });
     },
   },
 };
